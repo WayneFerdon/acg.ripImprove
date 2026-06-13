9653bc78af70315717baf66007fc1378b0116e89
WayneFerdon <wayneferdon@hotmail.com>
2026-06-13 18:28:01 +0800

    Update version and improve tracking logic

--- acg.ripImprove.user.js
+++ acg.ripImprove.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         acg.ripImprove
 // @namespace    http://tampermonkey.net/
-// @version      2026.06.04
+// @version      2026.06.12
 // @description  acg.rip torrent auto download
 // @author       WayneFerdon
 // @include        *acg.rip*
+// @downloadURL https://github.com/WayneFerdon/acg.ripImprove/raw/refs/heads/main/acg.ripImprove.user.js
 // @updateURL https://github.com/WayneFerdon/acg.ripImprove/raw/refs/heads/main/acg.ripImprove.user.js
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -15,27 +16,26 @@ const _1s = 1000;
 const _1m = 60 * _1s;
 const _1h = 60 * _1m;
 const _1d = 24 * _1h;
-
-const trackingItems = GM_getValue('tracking') ?? {};
-const tracking = Object.keys(trackingItems).map(t => t.match(/^\/.*\/$/) ? new RegExp(t.replace(/^\/|\/$/g, '')) : t);
-const $ajax = initAjax();
 const colors = { last: 'palegreen', tracking: 'crimson', downloaded: 'rebeccapurple' }
-let last = GM_getValue('last', null);
-let lastDownload = GM_getValue('lastDownload')
-let downloaded = GM_getValue('trackingDownloaded', );
-Object.keys(downloaded).forEach(k => {
-  if (tracking.map(t=>t.source ?? t).includes(k)) return;
-  delete downloaded[k];
-});
+const $ajax = initAjax();
+let trackingItems, tracking, downloaded, last, lastDownload;
 
-autoReload();
 onHandle();
 new MutationObserver(mutations => mutations.forEach(onHandle)).observe(document.documentElement, { childList: true });
+autoReload();
+
+function loadDatas() {
+  trackingItems = GM_getValue('tracking') ?? {};
+  tracking = Object.keys(trackingItems).map(t => t.match(/^\/.*\/$/) ? new RegExp(t.replace(/^\/|\/$/g, '')) : t);
+  downloaded = Object.fromEntries(Object.entries(GM_getValue('trackingDownloaded')).filter(([k, v]) => tracking.map(t=>t.source ?? t).includes(k)));
+  last = GM_getValue('last');
+  lastDownload = GM_getValue('lastDownload');
+}
 
 function onHandle() {
   onHandleItems(setDisplayHighlight);
   setTimeHover();
-  if (!window.location.href.endsWith('.rip/')) return;
+  if (!window.location.href.replace('page/1', '').endsWith('.rip/')) return;
   asyncDownloadTorrents();
 }
 
@@ -73,25 +73,14 @@ function setDisplayHighlight({ item, a, found, href }) {
 }
 
 async function asyncDownloadTorrents () {
-  let page = 1, html, done;
-  const url2num = (url) => 1*url.replace('/t/', '');
-  while (!done && !html?.includes(last) && page <= 20) {
-    if (page === 1) {
-      console.log('on index page');
-      html = document.documentElement.outerHTML;
-    } else {
-      console.log('fetching page:', page);
-      html = await $ajax.fetch(`/page/${page}`);
-    }
-    page++;
-    let delay = 0;
+  let page = 0, html, done;
+  const url2num = url => 1*url.replace('/t/', '');
+  while (!done && !html?.includes(last) && page++ <= 10) {
+    console.log('fetching page:', page);
+    html = page === 1 ? document.documentElement.outerHTML : await $ajax.fetch(`/page/${page}`);
     onHandleItems(({ item, found, title, href })=> {
-      if (url2num(href) <= url2num(last)) {
-        done = true;
-        return;
-      }
-      if (!found || downloaded[found]?.includes(href)) return;
-      console.log('download:', href, title, item, gE('.action>a', item));
+      if (done = url2num(href) <= url2num(last)) return true;
+      if (!found || downloaded[found]?.includes(href)) return false;
       window.open(href + '.torrent');//, '_self');
       (downloaded[found] ??= []).push(href);
       GM_setValue('lastDownload', found);
@@ -99,79 +88,79 @@ async function asyncDownloadTorrents () {
   }
 
   const newLast = gE('.title .title a', gE('tr', 'all')[1]).getAttribute('href');
-  if (url2num(newLast) > url2num(last)) {
-    GM_setValue('last', newLast);
-  }
+  if (url2num(newLast) > url2num(last)) GM_setValue('last', last = newLast);
   GM_setValue('trackingDownloaded', downloaded);
   onHandleItems(setDisplayHighlight);
 }
 
 async function autoReload() {
-  const date = new Date();
-  let [d,h,m,s] = [date.getDay(), date.getHours(), date.getMinutes(), date.getSeconds()];
-  const current = (((d*24+h)*60+m)*60+s)*_1s;
-  let min = Infinity;
-  let mini = undefined;
-  const index = gE('.post-index');
-  const parent = index.parentNode;
-  const display = cE('span');
-  parent.insertBefore(display, index);
+  const index = gE('.post-index'), display = cE('span');
+  index.parentNode.insertBefore(display, index);
 
-  for (let item in trackingItems) {
-    let time = trackingItems[item];
-    if (!time) continue;
-    item = item.match(/^\/.*\/$/) ? item.replace(/^\/|\/$/g, '') : item;
-    if (lastDownload === item) continue;
-    time = Math.floor(time/10000)*24*_1h+Math.floor((time/100)%100)*_1h+time%100*_1m;
-    const passed = current-time;
-    if (passed >= 30*_1m) continue;
-    if (passed < 0) {
-      min = Math.min(-passed, min);
-      mini = min === -passed ? item : mini;
-      continue;
+  let min = Infinity, mini, done;
+  (async () => {
+    while (!done) {
+      loadDatas();
+      const date = new Date();
+      let [d,h,m,s] = [(date.getDay()||7)-1, date.getHours(), date.getMinutes(), date.getSeconds()];
+      const current = (((d*_1d/_1h+h)*_1h/_1m+m)*_1m/_1s+s)*_1s;
+
+      for (let [item, time] of Object.entries(trackingItems).filter(([i,t])=>t)) {
+        item = item.match(/^\/.*\/$/) ? item.replace(/^\/|\/$/g, '') : item;
+        if (lastDownload === item) continue;
+        time = (Math.floor(time/100/100)+6)*_1d+Math.floor((time/100)%100)*_1h+time%100*_1m;
+        const remain = (time-current+2*_1h)%(7*_1d)-2*_1h;
+        if (remain < min) [min, mini] = [remain, item];
+        display.style.color = remain > 0 ? 'unset' : display.style.color='red';
+      };
+      if (min < 0) min = -min;
+      await pauseAsync(_1s);
     }
-    display.style.color='red';
-    min = passed;
-    mini = item;
-    break;
-  };
+  })();
 
-  let remain, waited = 0;
-  while ((remain=(min-waited)) > 0 && waited < _1h) {
-    const time = `${pad2(Math.floor(remain/_1h),' ')}:${pad2(Math.floor(remain%_1h/_1m))}:${pad2(Math.floor(remain%_1m/_1s))}`;
+  let remain, waited = 0, miniPrev=mini, minPrev=min;
+  while ((remain=(minPrev-waited)) > 0) {
+    if (mini !== miniPrev) [minPrev, miniPrev, waited] = [min, mini, 0];
+    const time = `${pad(Math.floor(remain/_1h),' ')}:${pad(Math.floor(remain%_1h/_1m))}:${pad(Math.floor(remain%_1m/_1s))}`;
     document.title = time;
     display.innerText = `\n ${time} ${mini}`;
-    waited+=_1s;
+    waited += _1s;
     await pauseAsync(_1s);
   }
+  done = true;
   window.location = window.location.href;
 }
 
-function pad2(num, pad='0') {
-  return num.toString().padStart(2, pad);
+function pad(num, pad='0', total=2) {
+  return num.toString().padStart(total, pad);
 }
 
 function onHandleItems(method, doc) {
-  gE('tr', 'all', doc??document).forEach(item => {
+  loadDatas();
+  for (const item of gE('tr', 'all', doc??document)) {
     const a = gE('.title .title a', item);
-    if (!a) return;
+    if (!a) continue;
     const [href, title] = [a.getAttribute('href'), a.innerHTML.replace(/\s+/g, ' ')];
     let found = tracking.find(t => t instanceof RegExp ? title.match(t) : title.includes(t));
     found = found?.source ?? found;
-    method({ item, a, found, title, href });
-  });
+    if (method({ item, a, found, title, href })) return;
+  }
 }
+
 function pauseAsync(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 function $doc(h) {
   const doc = document.implementation.createHTMLDocument('');
   doc.documentElement.innerHTML = h;
   return doc;
 }
+
 function cE(name) { // 创建元素
   return document.createElement(name);
 }
+
 function gE(ele, mode, parent) { // 获取元素
   if (typeof ele === 'object') {
     return ele;
@@ -183,6 +172,7 @@ function gE(ele, mode, parent) { // 获取元素
     return mode.querySelector(ele);
   }
 }
+
 function initAjax() {
   const $ajax = {
     debug: false,
@@ -292,10 +282,10 @@ function initAjax() {
       const text = r.responseText;
       if (r.status !== 200) {
         $ajax.error = `${r.status} ${r.statusText}: ${r.finalUrl}`;
-        r.context.onerror?.();
+        r.context.onerror?.(new Error($ajax.error));
       } else if (text === 'state lock limiter in effect') {
         $ajax.error = text;
-        r.context.onerror?.();
+        r.context.onerror?.(new Error($ajax.error));
       } else {
         r.context.onload?.(text);
         $ajax.next();
@@ -304,7 +294,7 @@ function initAjax() {
     onerror: function (r) {
       $ajax.conn--;
       $ajax.error = `${r.status} ${r.statusText}: ${r.finalUrl}`;
-      r.context.onerror?.();
+      r.context.onerror?.(new Error($ajax.error));
       $ajax.next();
     },
   };
